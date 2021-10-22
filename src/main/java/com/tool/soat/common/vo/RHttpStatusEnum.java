450e54d9c5d014322f4d0b7952b929eaf618b6b9
Sail <m17600576201@163.com>
2021-10-22 17:53:27 +0800

    fixed

--- src/main/java/com/tool/soat/common/vo/RHttpStatusEnum.java
+++ src/main/java/com/tool/soat/common/vo/RHttpStatusEnum.java
@@ -22,7 +22,7 @@ public enum RHttpStatusEnum {
     ADD_FAIL(40007,"新增用户失败"),
     USER_EXIST(40009,"用户已经存在"),
     USER_NOT_EXIST(40010,"用户不存在"),
-    CHANGE_STATUS_FAIL(40008,"新增用户失败"),
+    CHANGE_STATUS_FAIL(40008,"修改用户状态失败"),
     ADD_USER_REPETITION(40006,"邮箱或者电话不可重复"),
     NAV_QUERY_FAIL(40004,"获取导航栏失败");
 
